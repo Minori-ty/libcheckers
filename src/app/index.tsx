af12fff083c8c226ec9e4aa485969952a492231f
天平 <79521943+Minori-ty@users.noreply.github.com>
2026-07-06 08:59:42 +0000

    feat: add react-native-gesture-handler

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,9 @@
-import { Text, View, StyleSheet } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import Guster from 'react-native-gesture-handler';
 
 export default function Index() {
+  console.log(Guster);
+
   return (
     <View style={styles.container}>
       <Text>Edit src/app/index.tsx to edit this screen.</Text>
